ba277f30dab86eef81ebf63ac2e268b8353e80e8
BimoSK <elnonino23@gmail.com>
2024-11-01 01:20:34 -0600

    No se pero por si acaso

--- obj/Debug/net8.0/Free Zorrara.AssemblyInfo.cs
+++ obj/Debug/net8.0/Free Zorrara.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Free Zorrara")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ca46614f1932875346d3a17d698d08546e91b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Free Zorrara")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Free Zorrara")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
